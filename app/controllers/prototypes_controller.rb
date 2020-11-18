fcdae876057f4f3c208276d519cb2eab8118dfd3
kiserukun <73691576+kiserukun@users.noreply.github.com>
2020-11-18 20:38:23 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,57 @@
+class PrototypesController < ApplicationController
+
+  before_action :authenticate_user!,except: [:index, :show]
+
+  def index
+    @prototype = Prototype.includes(:user)
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show 
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+  
+  def edit
+    @prototype = Prototype.find(params[:id])
+    unless current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    @prototype.update(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render edit_prototype_path
+    end
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    @prototype.destroy
+    redirect_to root_path
+  end
+
+private
+
+  def prototype_params
+    params.require(:prototype).permit(:title,:image,:catch_copy,:concept).merge(user_id: current_user.id)
+  end
+  
+end
