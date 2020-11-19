ae3d87519ac1a635f93136e17f65b0903a6eedc1
kiserukun <73691576+kiserukun@users.noreply.github.com>
2020-11-19 18:00:20 +0900

    second commit

--- db/schema.rb
+++ db/schema.rb
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 2020_11_17_053810) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "encrypted_password", null: false
     t.text "position", null: false
     t.text "occupation", null: false
     t.text "profile", null: false
